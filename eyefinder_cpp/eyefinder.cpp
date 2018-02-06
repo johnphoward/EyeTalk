154f9e2a847298546bf65b6d030f56ed44e21a4d
Jae Kim <jaehoonkim1995@gmail.com>
2018-02-06 18:15:43 -0500

    Implemented pupil tracker leveraing EyeLike

--- eyefinder_cpp/eyefinder.cpp
+++ eyefinder_cpp/eyefinder.cpp
@@ -16,7 +16,7 @@ _EF_::EyeFinder::EyeFinder(void) : cap(0) {
   sem = sem_open(sem_name, O_CREAT | O_EXCL, 0666, 1);
   if (sem == SEM_FAILED) {
     std::cout << "*** *** sem_open failed!!!" << std::endl;
-    std::cout << sem_name << ", errno: " << errno <<std::endl;
+    std::cout << sem_name << ", errno: " << errno << std::endl;
     sem = sem_open(sem_name, O_CREAT, 0666, 1);
     sem_close(sem);
     exit(1);
@@ -27,7 +27,7 @@ _EF_::EyeFinder::EyeFinder(void) : cap(0) {
   // Initialize the Shared Memory (System V shared Memory)
   // Setup shared memory
   if ((shmid = shmget(key, shared_size, IPC_EXCL | IPC_CREAT | 0666)) < 0) {
-    std::cout << "*** *** shmget() failed!!! "<< errno << std::endl;
+    std::cout << "*** *** shmget() failed!!! " << errno << std::endl;
     exit(1);
   } else { // GOLDEN!!!
     std::cout << "shmid: " << shmid << std::endl;
@@ -70,8 +70,8 @@ int _EF_::EyeFinder::start(void) {
 
     // Grab and process frames until the main window is closed by the user.
     int i = 0;
-    while (true)
-    {
+    cv::namedWindow("LALALA", cv::WINDOW_AUTOSIZE);
+    while (i++ < 200) {
       // Grab a frame
       cv::Mat temp;
       if (!cap.read(temp)) {
@@ -87,7 +87,6 @@ int _EF_::EyeFinder::start(void) {
       // Detect faces
       std::vector<dlib::rectangle> faces = detector(cimg);
 
-
       // Find the pose of each face. (Only the first)
       std::vector<dlib::full_object_detection> shapes;
       for (unsigned long i = 0; i < 1 && i < faces.size(); ++i)
@@ -95,38 +94,25 @@ int _EF_::EyeFinder::start(void) {
 
       // guarantee that there are at least
       if (shapes.size()) {
-        // left eye + right eye
-        std::tuple<long, long, long, long> l_tp =
-            EyeFinder::setMinAndMax(36, 41, shapes);
-        std::tuple<long, long, long, long> r_tp =
-            EyeFinder::setMinAndMax(42, 47, shapes);
-
-        // ROI for left eye + right eye
-        cv::Rect roi_l = EyeFinder::getROI(l_tp, temp);
-        cv::Rect roi_r = EyeFinder::getROI(r_tp, temp);
-
-        // Display eye tracking on the screen
-        cv::Mat roi_l_mat, roi_r_mat;
-        roi_l_mat = temp(roi_l);
-        roi_r_mat = temp(roi_r);
 
         std::vector<long> facial_features_vec;
 
         // Left eye + Right eye points
-        preCalculationPoints(facial_features_vec, shapes);
+        preCalculationPoints(shapes, facial_features_vec);
 
         // Find the face angle + pupils
         calculateFaceAngles(shapes, facial_features_vec);
 
-        calculatePupils(roi_l_mat, facial_features_vec);
-        calculatePupils(roi_r_mat, facial_features_vec);
+        // calculatePupils(roi_l_mat, facial_features_vec);
+        // calculatePupils(roi_r_mat, facial_features_vec);
+        // Calling EyeLike's now
+        calculatePupilsEL(shapes, facial_features_vec, temp);
 
         // Write out the Facial Features
         writeFacialFeaturesToShm(facial_features_vec);
       } else {
         writeBadFacialFeaturesToShm(); // or skip it
       }
-
     }
   } catch (dlib::serialization_error &e) {
     std::cout << "You need dlib's default face landmarking model file to run "
@@ -154,7 +140,7 @@ PRIVATE
 // *****
 // setMinAndMax
 std::tuple<long, long, long, long> _EF_::EyeFinder::setMinAndMax(
-    int start, int end, std::vector<dlib::full_object_detection> &shapes) {
+    int start, int end, const std::vector<dlib::full_object_detection> &shapes) {
   // min_x, min_y, max_x, max_y
   std::tuple<long, long, long, long> tp{LONG_MAX, LONG_MAX, LONG_MIN, LONG_MIN};
 
@@ -202,8 +188,8 @@ cv::Rect _EF_::EyeFinder::getROI(std::tuple<long, long, long, long> &tp,
 //  0-11 = x, y coordinates of left eye points
 //  12-23 = x, y coordinates of right eye points
 void _EF_::EyeFinder::preCalculationPoints(
-    std::vector<long> &facial_features_vec,
-    const std::vector<dlib::full_object_detection> &shapes) {
+    const std::vector<dlib::full_object_detection> &shapes,
+    std::vector<long> &facial_features_vec) {
 
   for (int i = 36; i <= 47; ++i) {
     auto shp = shapes[0].part(i);
@@ -214,8 +200,9 @@ void _EF_::EyeFinder::preCalculationPoints(
 
 // *****
 // calculateFaceAngles()
-void _EF_::EyeFinder::calculateFaceAngles(const std::vector<dlib::full_object_detection> &shapes,
-                                          std::vector<long> &facial_features_vec) {
+void _EF_::EyeFinder::calculateFaceAngles(
+    const std::vector<dlib::full_object_detection> &shapes,
+    std::vector<long> &facial_features_vec) {
   auto left_cheek = shapes[0].part(1);
   auto right_cheek = shapes[0].part(16);
   auto nose_center = shapes[0].part(33);
@@ -226,46 +213,47 @@ void _EF_::EyeFinder::calculateFaceAngles(const std::vector<dlib::full_object_de
   float true_center_nose_x = left_cheek.x() + face_width / 2.0;
   float nose_x_offset = nose_center.x() - true_center_nose_x;
 
-
   float theta = 0.0;
   if (nose_x_offset != 0.0) {
     float angle = atan(nose_length / std::abs(nose_x_offset));
 
-    std::cout << "angle: " << angle << std::endl;
     theta = (90 - angle * (180.0 / M_PI));
-    std::cout << "theta: " << theta << std::endl;
     if (signbit(nose_x_offset))
       theta *= -1;
+#if DEBUG
+    std::cout << "angle: " << angle << std::endl;
+    std::cout << "theta: " << theta << std::endl;
+#endif
   }
-
 
   float vertical_cheek_offset = right_cheek.y() - left_cheek.y();
   float angle = atan(std::abs(vertical_cheek_offset) / face_width);
   float alpha = angle * (180.00 / M_PI);
   if (signbit(vertical_cheek_offset))
-      alpha *= -1;
+    alpha *= -1;
 
   // TODO: handle types for theta and alpha
   facial_features_vec.push_back(theta);
   facial_features_vec.push_back(alpha);
-
 }
 
 // *****
 // calculatePupils() a.k.a. Timm-Barth Algorithm
-void _EF_::EyeFinder::calculatePupils(cv::Mat src, std::vector<long> &facial_features_vec) {
+void _EF_::EyeFinder::calculatePupils(cv::Mat src,
+                                      std::vector<long> &facial_features_vec) {
 
   cv::Mat src_blur, src_blur_inv;
   int scale = 1;
   int delta = 0;
   int ddepth = CV_64F;
 
-  cv::GaussianBlur( src, src_blur, cv::Size(5,5), 0, 0, cv::BORDER_DEFAULT );
-  src_blur_inv = cv::Mat::ones(src_blur.size(), src_blur.type()) * 255 - src_blur;
+  cv::GaussianBlur(src, src_blur, cv::Size(5, 5), 0, 0, cv::BORDER_DEFAULT);
+  src_blur_inv =
+      cv::Mat::ones(src_blur.size(), src_blur.type()) * 255 - src_blur;
 
   cv::Mat grad_x, grad_y;
-  cv::Sobel( src, grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT );
-  cv::Sobel( src, grad_y, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT );
+  cv::Sobel(src, grad_x, ddepth, 1, 0, 3, scale, delta, cv::BORDER_DEFAULT);
+  cv::Sobel(src, grad_y, ddepth, 0, 1, 3, scale, delta, cv::BORDER_DEFAULT);
 
   cv::Mat mag(grad_x.size(), grad_x.type());
   cv::magnitude(grad_x, grad_y, mag);
@@ -279,28 +267,25 @@ void _EF_::EyeFinder::calculatePupils(cv::Mat src, std::vector<long> &facial_fea
   int col_LIMIT = src_blur_inv.cols;
 
   double max_objective = -777777.777;
-  int max_r=0, max_c=0;
+  int max_r = 0, max_c = 0;
 
-  for (int pixel_r = 10; pixel_r < row_LIMIT-10; ++pixel_r)
-  {
-    for (int pixel_c = 10; pixel_c < col_LIMIT-10; ++pixel_c)
-    {
+  for (int pixel_r = 10; pixel_r < row_LIMIT - 10; ++pixel_r) {
+    for (int pixel_c = 10; pixel_c < col_LIMIT - 10; ++pixel_c) {
       double obj_val = 0.0;
-      for (int i = 0; i < row_LIMIT; ++i)
-      {
-        cv::Vec3b* r_grad_x = grad_x.ptr<cv::Vec3b>(i);
-        cv::Vec3b* r_grad_y = grad_y.ptr<cv::Vec3b>(i);
-        cv::Vec3b* r_src_gray_blur_inv = src_blur_inv.ptr<cv::Vec3b>(i);
-        for (int j = 0; j < col_LIMIT; ++j)
-        {
+      for (int i = 0; i < row_LIMIT; ++i) {
+        cv::Vec3b *r_grad_x = grad_x.ptr<cv::Vec3b>(i);
+        cv::Vec3b *r_grad_y = grad_y.ptr<cv::Vec3b>(i);
+        cv::Vec3b *r_src_gray_blur_inv = src_blur_inv.ptr<cv::Vec3b>(i);
+        for (int j = 0; j < col_LIMIT; ++j) {
           int x_val = i - pixel_r;
           int y_val = j - pixel_c;
-          if ((x_val | y_val) == 0) break;
-          double magnitude = sqrt(x_val*x_val + y_val*y_val);
-          double unit_x = x_val/magnitude;
-          double unit_y = y_val/magnitude;
+          if ((x_val | y_val) == 0)
+            break;
+          double magnitude = sqrt(x_val * x_val + y_val * y_val);
+          double unit_x = x_val / magnitude;
+          double unit_y = y_val / magnitude;
 
-          double dot = (unit_x*r_grad_x[j][0] + unit_y*r_grad_y[j][0]);
+          double dot = (unit_x * r_grad_x[j][0] + unit_y * r_grad_y[j][0]);
           double dot2 = dot * dot;
 
           double pixel_val = r_src_gray_blur_inv[j][0] * dot2;
@@ -321,6 +306,63 @@ void _EF_::EyeFinder::calculatePupils(cv::Mat src, std::vector<long> &facial_fea
 }
 
 // *****
+// calculatePupilsEL() a.k.a. Timm-Barth Algorithm using EyeLike
+void _EF_::EyeFinder::calculatePupilsEL(
+    const std::vector<dlib::full_object_detection> &shapes,
+    std::vector<long> &facial_features_vec, cv::Mat temp) {
+
+  // left eye + right eye
+  std::tuple<long, long, long, long> l_tp =
+      EyeFinder::setMinAndMax(36, 41, shapes);
+  std::tuple<long, long, long, long> r_tp =
+      EyeFinder::setMinAndMax(42, 47, shapes);
+  std::tuple<long, long, long, long> face_tp =
+      EyeFinder::setMinAndMax(1, 16, shapes);
+
+  // ROI for left eye + right eye
+  cv::Rect roi_l = EyeFinder::getROI(l_tp, temp);
+  cv::Rect roi_r = EyeFinder::getROI(r_tp, temp);
+  cv::Rect roi_face = EyeFinder::getROI(face_tp, temp);
+
+  // Display eye tracking on the screen
+  cv::Mat roi_l_mat, roi_r_mat, roi_face_mat;
+  roi_l_mat = temp(roi_l);
+  roi_r_mat = temp(roi_r);
+  roi_face_mat = temp(roi_face);
+
+  cv::Mat temp_clone = temp.clone();
+  cv::GaussianBlur(temp_clone, temp_clone, cv::Size(0, 0),
+                   0.005 * roi_face.width);
+  cv::Point leftPupil = findEyeCenter(temp_clone, roi_l, "Left Eye");
+  cv::Point rightPupil = findEyeCenter(temp_clone, roi_r, "Right Eye");
+
+  int real_leftPupil_x = leftPupil.x + roi_l.x;
+  int real_leftPupil_y = leftPupil.y + roi_l.y;
+  int real_rightPupil_x = rightPupil.x + roi_r.x;
+  int real_rightPupil_y = rightPupil.y + roi_r.y;
+
+  // facial_features_vec.push_back((long)real_leftPupil_x);
+  // facial_features_vec.push_back((long)real_leftPupil_y);
+  // facial_features_vec.push_back((long)real_rightPupil_x);
+  // facial_features_vec.push_back((long)real_rightPupil_y);
+
+  facial_features_vec.push_back((long)-777);
+  facial_features_vec.push_back((long)-777);
+  facial_features_vec.push_back((long)-777);
+  facial_features_vec.push_back((long)-777);
+
+#if DEBUG_TB
+  cv::circle(temp, cv::Point(real_leftPupil_x, real_leftPupil_y), 1,
+             cv::Scalar(255, 255, 255, 255));
+  cv::circle(temp, cv::Point(real_rightPupil_x, real_rightPupil_y), 1,
+             cv::Scalar(255, 255, 255, 255));
+  // cv::resize(temp, temp, cv::Size(300,300));
+  cv::imshow("LALALA", temp);
+  cv::waitKey(1);
+#endif
+}
+
+// *****
 // writeFacialFeaturesToShm()
 //  Will be to shared memory with semaphore synchronization
 //  0-11 = x, y coordinates of left eye points
@@ -330,25 +372,24 @@ void _EF_::EyeFinder::calculatePupils(cv::Mat src, std::vector<long> &facial_fea
 //  28-29 = face angles (another code)
 void _EF_::EyeFinder::writeFacialFeaturesToShm(
     const std::vector<long> &facial_features_vec) {
-    int i = 0;
-    sem_wait(sem);
+  int i = 0;
+  sem_wait(sem);
 
-    // add an ID to the frame first
-    memcpy(shared_memory+sizeof(long)*i, &frame_id, sizeof(long));
-    i = 1;
+  // add an ID to the frame first
+  memcpy(shared_memory + sizeof(long) * i, &frame_id, sizeof(long));
+  i = 1;
 
-    // now copy every value in vec over
-    for (const auto num : facial_features_vec) {
-        memcpy(shared_memory+sizeof(long)*i, &num, sizeof(long));
-        i++;
-    }
+  // now copy every value in vec over
+  for (const auto num : facial_features_vec) {
+    memcpy(shared_memory + sizeof(long) * i, &num, sizeof(long));
+    i++;
+  }
 
-    // TODO: check if implementation has to change to represent face angles as floats
+  // TODO: check if implementation has to change to represent face angles as
+  // floats
 
-    sem_post(sem);
-    frame_id = (frame_id + 1) % 100;
+  sem_post(sem);
+  frame_id = (frame_id + 1) % 100;
 }
-
-
 
 void _EF_::EyeFinder::writeBadFacialFeaturesToShm(void) {}
--- eyefinder_cpp/eyefinder.h
+++ eyefinder_cpp/eyefinder.h
@@ -10,24 +10,39 @@
 
 #include "opencv2/imgproc/imgproc.hpp"
 
+#include <cmath>
 #include <string>
 #include <tuple>
 #include <utility>
 #include <vector>
-#include <cmath>
 
 // C headers
 // link with -lpthread
 #include <fcntl.h>
 #include <semaphore.h>
+#include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <stdlib.h>
 #include <unistd.h>
 
+// EyeLike
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+#include <iostream>
+#include <math.h>
+#include <queue>
+#include <stdio.h>
+
+#include "constants.h"
+#include "findEyeCenter.h"
+#include "findEyeCorner.h"
+
 #define DEBUG 0
+#define DEBUG_TB 1
 #define MACRO_START (begin = std::chrono::steady_clock::now())
 #define MACRO_END (end = std::chrono::steady_clock::now())
 #define MACRO_P_DIFF(MSG)                                                      \
@@ -38,55 +53,56 @@
              << std::endl)
 namespace _EF_ {
 
-    class EyeFinder final {
+class EyeFinder final {
 
-        public:
-          EyeFinder(void);
-          ~EyeFinder(void);
-          // Deleted so blocked from use
-          EyeFinder(const EyeFinder &other) = delete;
-          EyeFinder(EyeFinder &&other) = delete;
-          EyeFinder &operator=(const EyeFinder &other) = delete;
-          EyeFinder &operator=(EyeFinder &&other) = delete;
+public:
+  EyeFinder(void);
+  ~EyeFinder(void);
+  // Deleted so blocked from use
+  EyeFinder(const EyeFinder &other) = delete;
+  EyeFinder(EyeFinder &&other) = delete;
+  EyeFinder &operator=(const EyeFinder &other) = delete;
+  EyeFinder &operator=(EyeFinder &&other) = delete;
 
-          int start(void);
+  int start(void);
 
-        private:
-          const bool showmain = false;
-          const bool showeyes = false;
-          const bool clear = true;
-          static const unsigned int NUM_WINS = 3;
+private:
+  const bool showmain = false;
+  const bool showeyes = false;
+  const bool clear = true;
+  static const unsigned int NUM_WINS = 3;
 
-          cv::VideoCapture cap;
+  cv::VideoCapture cap;
 
-          sem_t *sem;
-          const char *sem_name = "/capstone";
+  sem_t *sem;
+  const char *sem_name = "/capstone";
 
-          long frame_id = 0;
+  long frame_id = 0;
 
-          int shmid;
-          char *shared_memory;
-          const key_t key = 123456;
-          const unsigned int shared_size =  2 * 30 * sizeof(long);
+  int shmid;
+  char *shared_memory;
+  const key_t key = 123456;
+  const unsigned int shared_size = sizeof(long) + 30 * sizeof(long);
 
-          // std::vector<long> facial_features_vec;
-          // change to python list later with boost, look below in start()
+  // std::vector<long> facial_features_vec;
+  // change to python list later with boost, look below in start()
 
-          std::tuple<long, long, long, long>
-          setMinAndMax(int start, int end,
-                       std::vector<dlib::full_object_detection> &shapes);
-          cv::Rect getROI(std::tuple<long, long, long, long> &tp, cv::Mat frame);
+  std::tuple<long, long, long, long>
+  setMinAndMax(int start, int end,
+               const std::vector<dlib::full_object_detection> &shapes);
+  cv::Rect getROI(std::tuple<long, long, long, long> &tp, cv::Mat frame);
 
-          void
-          preCalculationPoints(std::vector<long> &facial_features_vec,
-                               const std::vector<dlib::full_object_detection> &shapes);
-          void calculateFaceAngles( const std::vector<dlib::full_object_detection> &shapes,
-                                    std::vector<long> &facial_features_vec);
-          void calculatePupils(cv::Mat src,
-                               std::vector<long> &facial_features_vec);
-          void writeFacialFeaturesToShm(
-              const std::vector<long> &facial_features_vec);
-          void writeBadFacialFeaturesToShm(void);
-       };
+  void
+  preCalculationPoints(const std::vector<dlib::full_object_detection> &shapes,
+                       std::vector<long> &facial_features_vec);
+  void
+  calculateFaceAngles(const std::vector<dlib::full_object_detection> &shapes,
+                      std::vector<long> &facial_features_vec);
+  void calculatePupils(cv::Mat src, std::vector<long> &facial_features_vec);
+  void calculatePupilsEL(const std::vector<dlib::full_object_detection> &shapes,
+                         std::vector<long> &facial_features_vec, cv::Mat temp);
+  void writeFacialFeaturesToShm(const std::vector<long> &facial_features_vec);
+  void writeBadFacialFeaturesToShm(void);
+};
 }; // namespace _EF_
 #endif
